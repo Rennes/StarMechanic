d9294a1cc77b77029c2dfd5a59f400056ef17400
TORT 60091 <TORT60091@gmail.com>
2020-02-02 15:02:13 +0300

    Design improve

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour 
+{
+	public Vector3 rotation = Vector3.zero;
+	public bool startRotating = true;
+	void Update () 
+	{
+		if( startRotating )
+			transform.Rotate( rotation * Time.deltaTime );
+	}
+
+	public void EnableRotation()
+	{
+		startRotating = true;
+	}
+	public void DisableRotation()
+	{
+		startRotating = false;
+	}
+}
